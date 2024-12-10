eaf00889b49422cbd96aa082b35d04b0a929c630
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2024-12-11 01:01:31 +0400

    Created requirements, setup files

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+__version__ = "0.0.0"
+REPO_NAME = "disease-classification"
+AUTHOR_USER_NAME = "pradeep-kumar-r"
+SRC_REPO = "CNNClassifier"
+AUTHOR_EMAIL = "ravichandranpradeepkumar@gmail.com"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for a Deep Learning app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
